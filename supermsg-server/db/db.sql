027265760a89badc804dd77d7e8b2aca3ea7e509
finepix <499768985@qq.com>
2016-10-24 15:26:52 +0800

    change database schema.

--- supermsg-server/db/db.sql
+++ supermsg-server/db/db.sql
@@ -15,12 +15,13 @@ IF EXISTS user_baseinfo;
 
 CREATE TABLE `user_baseinfo` (
 	`id` VARCHAR (32) NOT NULL,
-	`phone` VARCHAR (11) NOT NULL,
 	`school_num` VARCHAR (15) NOT NULL,
+	`nation` VARCHAR (10) NOT NULL,
 	`st_num` VARCHAR (12) NOT NULL,
-	`mail` VARCHAR (50) NOT NULL,
 	`tag` VARCHAR (50) NOT NULL,
 	`pwd` VARCHAR (32) NOT NULL,
+	`sex` VARCHAR (4) NOT NULL,
+	`name` VARCHAR (10) NOT NULL,
 	PRIMARY KEY (`id`),
 	CONSTRAINT `FK_sch_num` FOREIGN KEY (`school_num`) REFERENCES `school_info` (`school_num`)
 );
@@ -30,11 +31,12 @@ IF EXISTS user_detailinfo;
 
 CREATE TABLE `user_detailinfo` (
 	`id` VARCHAR (32) NOT NULL,
-	`name` VARCHAR (20) NOT NULL,
+	`class` VARCHAR (20) NOT NULL,
 	`college` VARCHAR (20) NOT NULL,
 	`major` VARCHAR (30) NOT NULL,
 	`grade` VARCHAR (20) NOT NULL,
 	`phone` VARCHAR (11) NOT NULL,
+	`mail` VARCHAR (50) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
