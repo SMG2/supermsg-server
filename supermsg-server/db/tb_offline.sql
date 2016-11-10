7cdde820475d25bb549179d23e94f615aecd87f5
finepix <499768985@qq.com>
2016-11-10 21:50:55 +0800

    correct the database sql.

--- supermsg-server/db/tb_offline.sql
+++ supermsg-server/db/tb_offline.sql
@@ -2,9 +2,9 @@ USE db_chat;
 
 -- Create table --openfire聊天记录
 DROP TABLE
-IF EXISTS offLine_msg;
+IF EXISTS tb_person_msg;
 
-CREATE TABLE offLine_msg (
+CREATE TABLE tb_person_msg (
 	MESSAGEID INT PRIMARY KEY,
 	-- 消息id
 	SESSIONJID VARCHAR (30),
@@ -15,12 +15,24 @@ CREATE TABLE offLine_msg (
 	-- 接受者    
 	CREATEDATE VARCHAR (30),
 	-- 消息发送、创建时间
-	LENGTH INT,
-	-- 消息长度、大小
 	CONTENT VARCHAR (2000),
 	-- 消息内容
-	DETAIL VARCHAR (4000),
-	-- 消息源报文
-	STATE INT -- 删除状态，1表示删除
+	DETAIL VARCHAR (4000) -- 消息源报文
 );
 
+DROP TABLE
+IF EXISTS tb_group_msg;
+
+CREATE TABLE tb_group_msg (
+MESSAGEID INT PRIMARY KEY,
+ -- 消息id
+SESSIONJID VARCHAR (30),
+ -- 用户session jid名称
+SENDER VARCHAR (30),
+ -- 消息发送者
+CREATEDATE VARCHAR (30),
+ -- 消息发送、创建时间
+CONTENT VARCHAR (2000),
+ -- 消息内容
+DETAIL VARCHAR (4000) -- 消息源报文
+);
